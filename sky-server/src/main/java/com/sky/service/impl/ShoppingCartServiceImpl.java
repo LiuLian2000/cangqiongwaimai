6d204ffb7e4c5c4ee59dca1df6f99bba02e56359
liulian <1207186541@qq.com>
2024-07-13 20:13:20 +0800

    查看和清空购物车

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -59,4 +59,17 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
 
     }
+
+    @Override
+    public void cleanShoppingCart() {
+        shoppingCartMapper.deleteByUserId(BaseContext.getCurrentId());
+    }
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        return shoppingCartMapper.list(ShoppingCart.builder().
+                userId(BaseContext.getCurrentId()).
+                build());
+
+    }
 }
